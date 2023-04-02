77f30eb68faba43a92583f02c981bc0547919a43
ArsenRafayelyan <124182732+ArsenRafayelyan@users.noreply.github.com>
2023-04-02 12:11:12 +0400

    Add files via upload

---
+++ core/main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    name = models.CharField('Car name', max_length=30)
+    img = models.ImageField('Car image', upload_to='media')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
